50452a7ec056a91849ec3b4782d94f8d8bf29ab8
kym4 <patriciakym4@gmail.com>
2023-08-18 21:15:25 -0400

    cálculo trigonométrico

--- trigonometria.js
+++ trigonometria.js
@@ -1,12 +1,12 @@
-var trigonometria=prompt("escolhe\n 1.seno\n 2.coseno\n 3.tangete");
+var trigonometria=prompt("escolhe\n 1.seno\n 2.cosseno\n 3.tangente");
 var angulo=prompt("digite o angulo");
 switch (trigonometria){
-    case '1':
-     alert(math.sin((angulo*math.PI)/180).toFixed(2));break;
+    case'1':
+     alert(Math.sin((angulo*Math.PI)/180).toFixed(2));break;
      case'2':
-     alert(math.cos((angulos*math.P)/180));break;
+     alert(Math.cos((angulo*Math.PI)/180).toFixed(2));break;
      case'3':
-     alert(math.tan((angulos*math.PI)/180);break;
+     alert(Math.tan((angulo*Math.PI)/180).toFixed(2));break;
      default:
      alert("opçao invalida");
 }
