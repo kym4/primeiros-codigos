e206bd68fd574daaad6277d16ad25c7768c605cb
kym4 <patriciakym4@gmail.com>
2023-08-18 21:00:10 -0400

    primeiros codigos

---
+++ trigonometria.js
@@ -0,0 +1,12 @@
+var trigonometria=prompt("escolhe\n 1.seno\n 2.coseno\n 3.tangete");
+var angulo=prompt("digite o angulo");
+switch (trigonometria){
+    case '1':
+     alert(math.sin((angulo*math.PI)/180).toFixed(2));break;
+     case'2':
+     alert(math.cos((angulos*math.P)/180));break;
+     case'3':
+     alert(math.tan((angulos*math.PI)/180);break;
+     default:
+     alert("opçao invalida");
+}
